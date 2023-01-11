effa9b0fd707f22288b92b7b519012436fda6229
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-01-11 23:56:28 +0530

    initall commit

---
+++ hosting.js
@@ -0,0 +1,17 @@
+jsFuncrion(10)
+function jsFuncrion(x) {
+    console.log('====================================');
+    console.log(x);
+    console.log('====================================');
+}
+// varieable
+name = 'kamal';
+console.log(name); // kamal
+var name;
+
+// arrow function
+myFunction(); // . give error myFunction is not function
+var myFunction = () => {
+    console.log('Called');
+}
+
--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function sumWithArgument(arr) {
-    const args = arguments;
-    let sum = 0;
-    for (let i = 0; i < args.length; i++) {
-        sum += args[i];
-        
-    }
-    return sum
-}
-let result = sumWithArgument(1,2,3,4,5,6,7,8);
-console.log(result); // 38
+name = 'kamal';
+console.log(name);
+var name;
